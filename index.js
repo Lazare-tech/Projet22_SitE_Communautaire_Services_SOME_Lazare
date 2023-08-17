7d5dcd5c258c4833c243e74e7e345d98e749be6e
loliette <yelmaniyel@gmail.com>
2023-08-17 16:53:12 +0000

    disposition a revoir

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function myFunction() {
 function categorie(){
     
 }
+
+// disparition de carousel et les card au clic sur post
+let carousel=document.getElementById("carouselExampleCaptions")
+let freelanc_block=document.querySelector(".what_freelance")
+let become_freelance=document.querySelector(".become_freelance")
+let flex_content=document.querySelector(".flex-container")
+let block_regist=document.querySelector(".pos")
+// function post(){
+//   carousel.style.display="none";
+//   freelanc_block.style.display="none"
+//   become_freelance.style.display="none"
+//   flex_content.style.display="none"
+  
+// }
